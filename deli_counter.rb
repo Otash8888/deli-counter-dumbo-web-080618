336d2f292a1edd4941e6eac7215a25a454b12a5f
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 18:26:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,9 @@ end
 
 
 def take_a_number(katz_deli,name)
-katz_deli.each_with_index do |name, index|
-  
-"Welcome, Ada. You are number 1 in line."
+  katz_deli.each_with_index do |name, index|
+  puts "Welcome, #{name}. You are number #{index+1} in line."
+  end
 end
 
 now_serving ()
