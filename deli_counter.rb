c5340b97318125cc442a1402168068a716753279
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 18:38:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line (katz_deli)
   if katz_deli == []
   puts "The line is currently empty."
   else
-    
+   katz_deli_with_n = [] 
+   katz_deli.each_with_index
   "The line is currently: 1. Ada 2. Grace 3. Kent"
   end
   
